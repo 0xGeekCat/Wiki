2e916c13f2123e753e758483416ed9c04716efb6
0xGeekCat <1292881925@qq.com>
2021-07-12 11:15:41 +0800

    3-6 新增ebook表，并生成持久层代码

--- doc/all.sql
+++ doc/all.sql
@@ -1,10 +1,21 @@
-drop table if exists `test`;
-
-create table `test` (
+# 电子书表
+drop table if exists `ebook`;
+create table `ebook` (
+    # bigint对应Java的long
     `id` bigint not null comment 'ID',
     `name` varchar(50) comment '名称',
-    `password` varchar(50) comment '密码',
-    primary key(`id`)
-) engine=InnoDB default charset=utf8mb4 comment='测试';
+    `category1_id` bigint comment '分类1',
+    `category2_id` bigint comment '分类2',
+    `description` varchar(200) comment '描述',
+    `cover` varchar(200) comment '封面',
+    `doc_count` int comment '文档数',
+    `view_count` int comment '阅读量',
+    `vote_count` int comment '点赞数',
+    primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='电子书';
 
-insert into `test` (id, name, password) values(1, '测试', '12345');
+insert into `ebook` (id, name, description) values (1, 'Spring入门教程', '零基础学Spring开发');
+insert into `ebook` (id, name, description) values (2, 'Vue入门教程', '零基础学Vue开发');
+insert into `ebook` (id, name, description) values (3, 'Python入门教程', '零基础学Python开发');
+insert into `ebook` (id, name, description) values (4, 'Mysql入门教程', '零基础学Mysql开发');
+insert into `ebook` (id, name, description) values (5, 'Oracle入门教程', '零基础学Oracle开发');
